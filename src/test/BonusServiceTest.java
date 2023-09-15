0da216a375149cf5d365756756a9f9d2fdde6378
Clara <clara00017@hotmail.com>
2023-09-15 18:39:10 -0300

    abordagens de testes com exceção

--- src/test/BonusServiceTest.java
+++ src/test/BonusServiceTest.java
@@ -7,16 +7,20 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class BonusServiceTest {
 
     @Test
     public void bonusDeveSerZeroParaFuncionariosComSalarioAlto(){
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(),new BigDecimal("12000")));
-
-        assertEquals(new BigDecimal("0.00"),bonus);
+//        assertThrows(IllegalArgumentException.class,()-> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(),new BigDecimal("12000"))));
+        try {
+            service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(),new BigDecimal("12000")));
+            fail("Não deu a exception!");
+        }catch (Exception e){
+                assertEquals("Esse funcionário não pode receber bônus devido ao seu salário",e.getMessage() );
+        }
     }
     @Test
     public void bonusDeveSerDezPorcentoDoSalario(){
